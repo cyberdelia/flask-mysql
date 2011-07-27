2a85451d3559cf0848267cf9a3662b2d3f0f3fd8
Timothée Peignier <timothee.peignier@tryphon.org>
2011-07-27 19:28:02 +0200

    first import

---
+++ flaskext/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: UTF-8 -*-
+__import__('pkg_resources').declare_namespace(__name__)
---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+from setuptools import setup
+
+setup(
+    name='Flask-MySQL',
+    version='1.0',
+    url='https://github.com/cyberdelia/flask-mysql/',
+    license='BSD',
+    author=u'Timothée Peignier',
+    author_email='Timothée Peignier @example.com',
+    description='Flask simple mysql client',
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
