5efa13c648db30779ab5b5500648e4145966d4be
Timothée Peignier <timothee.peignier@icloud.com>
2024-11-13 20:17:25 -0800

    Bump package version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='Flask-MySQL',
-    version='1.5.2',
+    version='1.6.0',
     url='https://github.com/cyberdelia/flask-mysql/',
     license='BSD',
     author='Timothee Peignier',
